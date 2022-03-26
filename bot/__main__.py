bda755dd6a38f1a15a6bebf0332cad3f2c239b67
Maleesha Dilshan <99663018+Dilshan28@users.noreply.github.com>
2022-03-26 18:49:32 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -186,7 +186,7 @@ async def something():
                 aa = kk.split(".")[-1]
                 newFile = dl.replace(f"downloads/", "").replace(f"_", " ")
                 rr = "encode"
-                bb = kk.replace(f".{aa}", ".mkv")
+                bb = kk.replace(f".{aa}", "[HEVC].mkv")
                 out = f"{rr}/{bb}"
                 thum = "thumb.jpg"
                 dtime = ts(int((es - s).seconds) * 1000)
